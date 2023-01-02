d36d5c5ed063797c5c5fb93a97d044e4898be853
Eric Smith <eric@esmithy.net>
2023-01-02 12:27:37 -0700

    After props/state

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import * as React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  console.log('App renders');
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search onSearch={handleSearch}/>
+      <List list={stories} />
     </div>
-  )
+  );
 }
 
-export default App
+const Search = (props) => {
+  console.log('Search renders')
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    props.onSearch(event);
+  }
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange}></input>
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>);
+}
+
+const List = (props) => {
+  console.log('List renders')
+  return (
+    <ul>
+      {
+        props.list.map((item) => (
+          <Item key={item.objectID} item={item}/>
+        ))
+      }
+    </ul>
+  );
+}
+
+const Item = (props) => {
+  console.log('Item renders')
+  const item = props.item;
+  return (
+    <li>
+    <span><a href={item.url}>{item.title}</a> </span>
+    <span>{item.author} </span>
+    <span>{item.num_comments} </span>
+    <span>{item.points}</span>
+  </li>
+);
+}
+
+
+export default App;
