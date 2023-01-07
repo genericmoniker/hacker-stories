a0f4950757cf377fac39fbd2e7895b244d88fe6d
Eric Smith <eric@esmithy.net>
2023-01-07 15:43:47 -0700

    Through conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const useStorageState = (key, initialState) => {
 };
 
 const App = () => {
-  const stories = [
+  const initialStories = [
     {
       title: 'React',
       url: 'https://reactjs.org',
@@ -33,7 +33,33 @@ const App = () => {
     }
   ]
 
+  const getAsyncStories = () =>
+    new Promise((resolve) =>
+      setTimeout(
+        () => resolve({ data: { stories: initialStories } }),
+        2000
+      )
+    );
+
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+  const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+      setIsLoading(false);
+    }).catch(() => setIsError(true));
+  }, []); // Empty array means run once when component first renders.
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  }
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -48,32 +74,55 @@ const App = () => {
       <h1>My Hacker Stories</h1>
       <InputWithLabel
         id="search"
-        label="Search"
         value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
-      />
-      <List list={searchedStories} />
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
+      <hr />
+      {isError && <p>Something went wrong...</p>}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 }
 
-const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => {
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}) => {
   return (
     <div>
-      <label htmlFor={id}>{label}</label>
-      <input id={id} type={type} value={value} onChange={onInputChange}></input>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        id={id}
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange} />
     </div>);
 }
 
-const List = ({ list }) => (
-  <ul>
-    {list.map((item) => (
-      <Item key={item.objectID} item={item} />
-    ))}
-  </ul>
-);
+const List = ({ list, onRemoveItem }) => {
+  return (
+    <ul>
+      {list.map((item) => (
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+      ))}
+    </ul>
+  );
+}
 
-const Item = ({ item }) => (
+const Item = ({ item, onRemoveItem }) => (
   <li>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -81,6 +130,8 @@ const Item = ({ item }) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    &nbsp;
+    <button type="button" onClick={() => onRemoveItem(item)}>Dismiss</button>
   </li>
 );
 
