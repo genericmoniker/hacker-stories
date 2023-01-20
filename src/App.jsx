fa46311bf278b3692141b9e9d35b257d566dd4db
Eric Smith <eric@esmithy.net>
2023-01-19 20:10:25 -0700

    Real data!

--- src/App.jsx
+++ src/App.jsx
@@ -13,33 +13,9 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
-const App = () => {
-  const initialStories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    }
-  ]
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
-  const getAsyncStories = () =>
-    new Promise((resolve, reject) =>
-      setTimeout(reject, 2000)
-      // () => resolve({ data: { stories: initialStories } }),
-      // 2000
-      //)
-    );
+const App = () => {
 
   const storiesReducer = (state, action) => {
     switch (action.type) {
@@ -65,15 +41,22 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   );
 
+  // useEffect to hook into the lifecycle of the component.
   React.useEffect(() => {
+    // If there is no search term, then don't do anything.
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    getAsyncStories().then(result => {
+    fetch(`${API_ENDPOINT} ${searchTerm}`).then((response) => response.json())
+    .then((result) => {
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories,
+        payload: result.hits,
       });
-    }).catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-  }, []); // Empty array means run once when component first renders.
+    }).catch(() =>
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+    );
+  }, [searchTerm]); // Empty array would mean run once when component first renders.
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -85,10 +68,6 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  const searchedStories = stories.data.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <div>
@@ -106,7 +85,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   );
@@ -148,8 +127,8 @@ const Item = ({ item, onRemoveItem }) => (
     <span>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
+    <span>{item.author} </span>
+    <span>{item.num_comments} </span>
     <span>{item.points}</span>
     &nbsp;
     <button type="button" onClick={() => onRemoveItem(item)}>Dismiss</button>
